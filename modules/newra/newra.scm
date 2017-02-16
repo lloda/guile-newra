b16b3e756eca2f764b8138321140f6606956c722
lloda <lloda@sarc.name>
2017-02-16 18:39:45 +0100

    A crude constructor make-ra-c

--- modules/newra/newra.scm
+++ modules/newra/newra.scm
@@ -10,10 +10,10 @@
 (define-module (newra newra)
   #:export (make-ra ra? ra-data ra-zero ra-dims ra-vlen ra-vref ra-vset!
             make-dim dim? dim-len dim-lo dim-step dim-ref
-            ra-rank ra-type ra-lowest-index array->ra))
+            ra-rank ra-type ra-lowest-index array->ra make-ra-c))
 
-(import (srfi srfi-9) (srfi srfi-9 gnu) (srfi srfi-1) (srfi srfi-8)
-        (srfi srfi-4 gnu) (srfi srfi-26)
+(import (srfi srfi-9) (srfi srfi-9 gnu) (only (srfi srfi-1) fold) (srfi srfi-8)
+        (srfi srfi-4 gnu) (srfi srfi-26) (ice-9 match)
         (only (rnrs base) vector-map vector-for-each))
 
 (define (struct-length s)
@@ -61,45 +61,50 @@
 (define (ra? o)
   (and (struct? o) (eq? <ra-vtable> (struct-vtable o))))
 
-(define (make-ra* data zero dims vlen vref vset!)
-  (make-struct/no-tail
-   <ra-vtable>
-   (lambda i i)
-   data zero dims vlen vref vset!))
+(define (make-ra* data zero dims type vlen vref vset!)
+  (letrec ((ra
+            (make-struct/no-tail
+             <ra-vtable>
+             (lambda i (format (current-error-port) "you've called REF ra (~a) with args: ~a\n" ra i))
+             data zero dims type vlen vref vset!)))
+    ra))
 
-(define (ra-data a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 1))
-(define (ra-zero a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 2))
-(define (ra-dims a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 3))
-(define (ra-vlen a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 4))
-(define (ra-vref a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 5))
-(define (ra-vset! a) (unless (ra? a) (throw 'bad-ra a)) (struct-ref a 6))
+(define field0 1)
+(define (ra-data a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 0)))
+(define (ra-zero a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 1)))
+(define (ra-dims a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 2)))
+(define (ra-type a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 3)))
+(define (ra-vlen a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 4)))
+(define (ra-vref a)  (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 5)))
+(define (ra-vset! a) (unless (ra? a) (throw 'bad-ra a)) (struct-ref a (+ field0 6)))
 
 (define (pick-typed-vector-functions v)
-  (cond ((vector? v)    (values vector-length    vector-ref    vector-set!   ))
-        ((c64vector? v) (values c64vector-length c64vector-ref c64vector-set!))
-        ((c32vector? v) (values c32vector-length c32vector-ref c32vector-set!))
-        ((f64vector? v) (values f64vector-length f64vector-ref f64vector-set!))
-        ((f32vector? v) (values f32vector-length f32vector-ref f32vector-set!))
-        ((s64vector? v) (values s64vector-length s64vector-ref s64vector-set!))
-        ((s32vector? v) (values s32vector-length s32vector-ref s32vector-set!))
-        ((s16vector? v) (values s16vector-length s16vector-ref s16vector-set!))
-        ((s8vector? v)  (values s8vector-length  s8vector-ref  s8vector-set! ))
-        ((s64vector? v) (values s64vector-length s64vector-ref s64vector-set!))
-        ((s32vector? v) (values s32vector-length s32vector-ref s32vector-set!))
-        ((s16vector? v) (values s16vector-length s16vector-ref s16vector-set!))
-        ((s8vector? v)  (values s8vector-length  s8vector-ref  s8vector-set! ))
-        ((string? v)    (values string-length    string-ref    string-set!   ))
-        ((bitvector? v) (values bitvector-length bitvector-ref bitvector-set!))
+  (cond ((vector? v)    (values #t   vector-length    vector-ref    vector-set!   ))
+        ((c64vector? v) (values 'c64 c64vector-length c64vector-ref c64vector-set!))
+        ((c32vector? v) (values 'c32 c32vector-length c32vector-ref c32vector-set!))
+        ((f64vector? v) (values 'f64 f64vector-length f64vector-ref f64vector-set!))
+        ((f32vector? v) (values 'f32 f32vector-length f32vector-ref f32vector-set!))
+        ((s64vector? v) (values 's64 s64vector-length s64vector-ref s64vector-set!))
+        ((s32vector? v) (values 's32 s32vector-length s32vector-ref s32vector-set!))
+        ((s16vector? v) (values 's16 s16vector-length s16vector-ref s16vector-set!))
+        ((s8vector? v)  (values 's8  s8vector-length  s8vector-ref  s8vector-set! ))
+        ((u64vector? v) (values 'u64 s64vector-length s64vector-ref s64vector-set!))
+        ((u32vector? v) (values 'u32 s32vector-length s32vector-ref s32vector-set!))
+        ((u16vector? v) (values 'u16 s16vector-length s16vector-ref s16vector-set!))
+        ((u8vector? v)  (values 'u8  s8vector-length  s8vector-ref  s8vector-set! ))
+        ((string? v)    (values 'a   string-length    string-ref    string-set!   ))
+        ((bitvector? v) (values 'b   bitvector-length bitvector-ref bitvector-set!))
 ; @TODO extend this idea to 'non-strict arrays' (cf Racket), to a method for drag-along
-        ((dim? v)       (values  dim-len         dim-ref       (cut throw 'noo-dim-set! <...>)))
+        ((dim? v)       (values 'd   dim-len          dim-ref       (cut throw 'no-dim-set! <...>)))
         (else (throw 'bad-ra-data-type v))))
 
+; low level, for conversions
 (define (make-ra data zero dims)
   (unless (vector? dims) (throw 'bad-dims dims))
   (vector-for-each (lambda (dim) (unless (dim? dim) (throw 'bad-dim dim))) dims)
 ; after check
-  (receive (vlen vref vset!) (pick-typed-vector-functions data)
-    (make-ra* data zero dims vlen vref vset!)))
+  (receive (type vlen vref vset!) (pick-typed-vector-functions data)
+    (make-ra* data zero dims type vlen vref vset!)))
 
 ; for some types of loops, or to transition from Guile C arrays.
 (define (ra-lowest-index zero dims)
@@ -116,27 +121,6 @@
 (define (ra-rank a)
   (vector-length (ra-dims a)))
 
-(define (ra-type a)
-  (let ((v (ra-data a)))
-    (cond ((vector? v)    #t)
-          ((c64vector? v) 'c64)
-          ((c32vector? v) 'c32)
-          ((f64vector? v) 'f64)
-          ((f32vector? v) 'f32)
-          ((s64vector? v) 's64)
-          ((s32vector? v) 's32)
-          ((s16vector? v) 's16)
-          ((s8vector? v)  's8)
-          ((s64vector? v) 's64)
-          ((s32vector? v) 's32)
-          ((s16vector? v) 's16)
-          ((s8vector? v)  's8)
-          ((string? v)    'a)
-          ((bitvector? v) 'b)
-; @TODO extend this idea to 'non-strict arrays', to a method for drag-along
-          ((dim? v)       'd)
-          (else (throw 'bad-ra-data-type v)))))
-
 ; FIXME while we work on our constructors, then stop using out of tests.
 (define (array->ra a)
   (let ((dims (list->vector
@@ -147,3 +131,23 @@
     (make-ra (shared-array-root a)
              (- (shared-array-offset a) (ra-lowest-index 0 dims))
              dims)))
+
+(define (make-ra-c type value . d)
+  (let* ((dims (let loop ((d d))
+                 (match d
+                   (() '())
+                   (((lo hi)) (list (make-dim (- hi lo -1) lo 1)))
+                   ((len) (list (make-dim len 0 1)))
+                   (((lo hi) . rest)
+                    (let ((next (loop rest))
+                          (len (- hi lo -1)))
+                      (cons (make-dim len lo (* (dim-len (car next)) (dim-step (car next)))) next)))
+                   ((len . rest)
+                    (let ((next (loop rest)))
+                      (cons (make-dim len 0 (* (dim-len (car next)) (dim-step (car next)))) next))))))
+         (size (fold (lambda (a c) (* c (dim-len a))) 1 dims))
+         (dims (list->vector dims)))
+    (make-ra
+     (make-typed-array type value size)
+     (- (ra-lowest-index 0 dims))
+     dims)))
--- test.scm
+++ test.scm
@@ -3,8 +3,8 @@
 ; (c) Daniel Llorens - 2016-2017
 
 (import (newra newra) (newra print) (newra tools)
-        (only (rnrs base) vector-map)
-        (srfi srfi-26))
+        (only (rnrs base) vector-map) (only (srfi srfi-1) fold)
+        (srfi srfi-26) (ice-9 match))
 
 (define ra0 (make-ra (make-vector 10 3) 0 (vector (make-dim 3))))
 (define ra1 (make-ra (make-vector 10 3) 0 (vector (make-dim 3) (make-dim 3))))
@@ -17,8 +17,16 @@
 (define ra2 (array->ra #2((1 2) (3 4))))
 (define ra3 (array->ra #2@1@1((1 2) (3 4))))
 
-; ways to go
-(define array0 (make-typed-array 'f64 9 10 10 10 10 10 10))
-(define t0 (time (call-with-output-file "/dev/null" (cut display array0 <>))))
-(define t1 (time (call-with-output-file "/dev/null" (cut ra-print (array->ra array0) <>))))
-(define t2 (time (call-with-output-file "/dev/null" (cut (@@ (ice-9 arrays) array-print) array0 <>))))
+;; ; ways to go
+;; (define array0 (make-typed-array 'f64 9 10 10 10 10 10 2))
+;; (define t0 (time (call-with-output-file "/dev/null" (cut display array0 <>))))
+;; (define t1 (time (call-with-output-file "/dev/null" (cut ra-print (array->ra array0) <>))))
+;; (define t2 (time (call-with-output-file "/dev/null" (cut (@@ (ice-9 arrays) array-print) array0 <>))))
+
+(make-ra-c #t 0 2 3)
+(make-ra-c #t 0 3 2)
+(make-ra-c #t 0 '(1 3) 2)
+(define ra4 (make-ra-c #t 0 '(1 3) '(1 2)))
+(array-index-map! (ra-data ra4) (lambda i i))
+
+; now array-cell, etc.
