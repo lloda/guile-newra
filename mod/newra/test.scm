3d64aab0a513472676f061f1dfba34ef92702c44
lloda <lloda@sarc.name>
2017-02-28 12:10:54 +0100

    Merge printing benchmark
    * mod/newra/test.scm (array-print*): Filed from trials.scm.
    * bench.scm: Run benchmark.

--- mod/newra/test.scm
+++ mod/newra/test.scm
@@ -7,13 +7,14 @@
 ; later version.
 
 ;;; Commentary:
-;; Alternative implementations for testing and benchmarking.
+;; Alternative implementations (both for ra and array) for testing and benchmarking.
 ;;; Code:
 
 (define-module (newra test)
-  #:export (ra-map*! array-map*!))
+  #:export (ra-map*! array-map*! array-print*))
 
-(import (newra newra) (only (rnrs base) vector-map vector-for-each))
+(import (newra newra)
+        (only (rnrs base) vector-map vector-for-each) (rnrs io ports) (srfi srfi-4 gnu))
 
 (define ra-map*!
   (case-lambda
@@ -47,3 +48,52 @@
            (array-set! o (apply f (map array-ref args))))
          o args)
   o)
+
+(define array-print-prefix (@@ (ice-9 arrays) array-print-prefix))
+
+; this is a direct translation of scm_i_print_array_dimension() in arrays.c.
+(define (array-print* a port)
+  (define lo caar)
+  (define hi cadar)
+  (array-print-prefix a port)
+  (let ((s (array-shape a))
+        (i (shared-array-increments a))
+        (r (shared-array-root a))
+        (b (shared-array-offset a)))
+    (let ((ref (case (array-type r)
+                 ((#t) vector-ref)
+                 ((c64) c64vector-ref)
+                 ((c32) c32vector-ref)
+                 ((f64) f64vector-ref)
+                 ((f32) f32vector-ref)
+                 ((s64) s64vector-ref)
+                 ((s32) s32vector-ref)
+                 ((s16) s16vector-ref)
+                 ((s8)  s8vector-ref)
+                 ((u64) u64vector-ref)
+                 ((u32) u32vector-ref)
+                 ((u16) u16vector-ref)
+                 ((u8) u8vector-ref)
+                 ((a) string-ref)
+                 ((b) bitvector-ref)
+                 (else (throw 'bad-type (array-type r))))))
+; special case
+      (if (zero? (array-rank a))
+        (begin
+          (display #\( port)
+          (display (ref b) port)
+          (display #\) port))
+        (let loop ((ss s) (ii i) (b b))
+          (if (null? ss)
+            (display (ref r b) port)
+            (let ((lo (lo ss))
+                  (hi (hi ss))
+                  (i (car ii)))
+              (put-char port #\()
+              (do ((j lo (+ 1 j))
+                   (b b (+ b i)))
+                  ((> j hi))
+                (loop (cdr ss) (cdr ii) b)
+                (when (< j hi)
+                  (put-char port #\space)))
+              (put-char port #\)))))))))
