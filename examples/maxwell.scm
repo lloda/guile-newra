a129ed52519871415f5109403fe38e06b391bb64
lloda <lloda@sarc.name>
2020-09-04 12:40:11 +0200

    Graphical notation

--- examples/maxwell.scm
+++ examples/maxwell.scm
@@ -27,10 +27,12 @@
 
 (define I (ra-iota)) ; index for the first dimension
 (define ⍉ ra-transpose)
+(define .. (ldots))
+(define : (ldots 1))
 
 (define (draw t F)
-  (show t "Ey" (ra-from F t #t 0 0 2 0))
-  (show t "Bz" (ra-from F t #t 0 0 2 1))
+  (show t "Ey" (ra-from F t : 0 0 2 0))
+  (show t "Bz" (ra-from F t : 0 0 2 1))
   (usleep #e50e3))
 
 (define (show t name F)
@@ -53,18 +55,18 @@
          (Y (make-ra 0 n m l 4))
          (dn (* 2 (acos -1) (/ n))))
 
-    (ra-map! (ra-from A 0 (ldots) 2) (lambda (x) (* -1 (/ dn) (cos (* x dn)))) I)
-    (ra-map! (ra-from A 1 (ldots) 2) (lambda (x) (* -1 (/ dn) (cos (* (- x δ) dn)))) I)
+    (ra-map! (ra-from A 0 .. 2) (lambda (x) (* -1 (/ dn) (cos (* x dn)))) I)
+    (ra-map! (ra-from A 1 .. 2) (lambda (x) (* -1 (/ dn) (cos (* (- x δ) dn)))) I)
 
     (do ((t 1 (+ t 1))) ((= o (+ t 1)))
       (ra-map! X + (⌽ +1 0 (A t)) (⌽ +1 1 (A t)) (⌽ +1 2 (A t)))
       (ra-map! Y + (⌽ -1 0 (A t)) (⌽ -1 1 (A t)) (⌽ -1 2 (A t)))
       (ra-map! (A (+ t 1)) (lambda (x y am1 a) (+ x y (- am1) (* -4 a))) X Y (A (- t 1)) (A t)))
 
-    (ra-map! (ra-from DA (ldots) 0) (lambda (a b) (/ (- a b) +2 δ)) (⌽ 1 0 A) (⌽ -1 0 A))
-    (ra-map! (ra-from DA (ldots) 1) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 1 A) (⌽ -1 1 A))
-    (ra-map! (ra-from DA (ldots) 2) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 2 A) (⌽ -1 2 A))
-    (ra-map! (ra-from DA (ldots) 3) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 3 A) (⌽ -1 3 A))
+    (ra-map! (ra-from DA .. 0) (lambda (a b) (/ (- a b) +2 δ)) (⌽ 1 0 A) (⌽ -1 0 A))
+    (ra-map! (ra-from DA .. 1) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 1 A) (⌽ -1 1 A))
+    (ra-map! (ra-from DA .. 2) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 2 A) (⌽ -1 2 A))
+    (ra-map! (ra-from DA .. 3) (lambda (a b) (/ (- a b) -2 δ)) (⌽ 1 3 A) (⌽ -1 3 A))
 
     (format #t "'LORENTZ CONDITION: MAX|DIV| = 0? ~a'\n"
             (ra-fold (lambda (a b) (max (magnitude a) b))
