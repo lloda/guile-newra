85b816c55679ab162e7116920c1466f3f504e1f6
lloda <lloda@sarc.name>
2021-11-10 17:55:18 +0100

    Checking version of foreign-library-function for fortran args, wip

--- sandbox/ffi.scm
+++ sandbox/ffi.scm
@@ -48,10 +48,65 @@
 (lbounder (ra->fortran (ra-reshape (make-typed-ra 'f64 1. 3) 0 '(3 5)))) ; 1 :-\
 
 #|
+(fortran-library-function libexample "maybe" #:return-type int32
+                          #:arg-types '(double int (double : 3) (double_complex ..)))
+|#
+
+(define (ffi-type t)
+  (case t
+    ((vu8 u8) uint8)
+    ((u16) uint16)
+    ((u32) uint32)
+    ((u64) uint64)
+    ((s8) int8)
+    ((s16) int16)
+    ((s32) int32)
+    ((s64) int64)
+    ((f32) float)
+    ((f64) double)
+    ;; ((c32) CFI_type_float_Complex)
+    ;; ((c64) CFI_type_double_Complex)
+    (else (throw 'no-ffi-type-for t))))
+
+(define* (fortran-library-function lib name return-type arg-types)
+  (let ((f (foreign-library-function
+            lib name #:return-type return-type
+            #:arg-types (make-list (length arg-types) '*))))
+    (lambda args
+      (apply f
+        (map (match-lambda*
+               ((arg (type dims ...))
+                (unless (eqv? type (ffi-type (ra-type arg)))
+                  (throw 'bad-type type (ra-type arg)))
+                (ra->fortran
+                 (let ((ndims (length dims)))
+                   (if (and (= ndims 1) (eq? '.. (car ndims)))
+                     arg
+                     (if (and (= ndims (ra-rank arg))
+                              (every (lambda (lohi dim)
+                                       (or (eq? ': dim) (equal? lohi dim)))
+                                     (ra-dimensions arg) dims))
+                       arg
+                       (throw 'bad-sizes (ra-dimensions arg) dims))))))
+               ((arg '*)
+                arg)
+               ((arg type)
+                (make-c-struct (list type) (list arg))))
+          args
+          arg-types)))))
+
+(define lookup-xy* (fortran-library-function libexample "lookup_xy" double `(,double ,double (,double : :))))
+(lookup-xy* 1 2 table0)
+
+#|
 [x] fix rank 0
 [x] fix non-zero ra-offset
 [x] verify behavior of lbounds
 [ ] define something like fortran-library-function
+  [ ] fix arg-types format
+  [ ] bool
+  [ ] c32/c64
+  [ ] support out args, e.g. with boxes (?)
 [ ] support in/out/inout
 [ ] fix alignment assumptions (seems there's padding in some versions of ISO_Fortran_binding.h :-/)
 [ ] modulize, doc, etc.
